9d7329a07440e3c8cde4fa22d65e5fd8915a40be
tableminpark <tablemin_park@daum.net>
2024-11-22 10:51:57 +0900

    feat: ErrorException 구조 수정

--- src/main/java/com/monglife/core/exception/ErrorException.java
+++ src/main/java/com/monglife/core/exception/ErrorException.java
@@ -4,9 +4,22 @@ import com.monglife.core.enums.response.Response;
 
 import java.util.Map;
 
-public interface ErrorException {
+public class ErrorException extends RuntimeException {
 
-    Response getResponse();
+    private final Response response;
 
-    Map<String, ?> getResult();
+    private final Map<String, Object> result;
+
+    public ErrorException(Response response, Map<String, Object> result) {
+        this.response = response;
+        this.result = result;
+    }
+
+    public Response getResponse() {
+        return this.response;
+    }
+
+    public Map<String, Object> getResult() {
+        return this.result;
+    }
 }
