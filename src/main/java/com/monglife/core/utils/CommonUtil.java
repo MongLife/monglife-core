8725f4cc7304a2945d39151ffa9919d4353d146f
tableminpark <tablemin_park@daum.net>
2025-01-10 09:29:41 +0900

    feat: CommonUtil 추가

--- src/main/java/com/monglife/core/utils/CommonUtil.java
+++ src/main/java/com/monglife/core/utils/CommonUtil.java
@@ -4,10 +4,6 @@ import java.util.UUID;
 
 public class CommonUtil {
 
-    /**
-     * UUID random ID 발급
-     * @return random ID 문자열
-     */
     public static String randomId() {
         return UUID.randomUUID().toString().replace("-", "");
     }
