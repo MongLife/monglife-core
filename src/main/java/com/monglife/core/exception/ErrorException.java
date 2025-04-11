caff98a485b0c9e653c723e0c305992ee1e768aa
박상민 <tablemin_park@daum.net>
2025-04-11 17:17:14 +0900

    Release for v2.0.9 (#11)
    - 사용자 예외 클래스 필드 수정

--- src/main/java/com/monglife/core/exception/ErrorException.java
+++ src/main/java/com/monglife/core/exception/ErrorException.java
@@ -1,5 +1,6 @@
 package com.monglife.core.exception;
 
+import com.monglife.core.enums.error.ErrorCode;
 import com.monglife.core.enums.response.Response;
 import lombok.Getter;
 
@@ -8,7 +9,7 @@ import java.util.Map;
 @Getter
 public class ErrorException extends RuntimeException {
 
-    protected Response response;
+    protected ErrorCode errorCode;
 
     protected Map<String, Object> result;
 }
