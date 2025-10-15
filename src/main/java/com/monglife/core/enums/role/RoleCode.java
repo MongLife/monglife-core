d4f32a22ec0f6578e4fbb5cb8e7cb575fd56d4a7
tableminpark <tablemin_park@daum.net>
2025-10-15 21:29:07 +0900

    feat: 회원 분류 추가

--- src/main/java/com/monglife/core/enums/role/RoleCode.java
+++ src/main/java/com/monglife/core/enums/role/RoleCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RoleCode {
 
+    ANONYMOUSE("ANONYMOUSE", "비회원"),
     NORMAL("NORMAL", "일반회원"),
+    SUBSCRIBER("SUBSCRIBER", "구독회원"),
     ADMIN("ADMIN", "관리자"),
     ;
 
